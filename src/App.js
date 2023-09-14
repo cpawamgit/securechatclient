8081b803133aeee0f1d84f32d685326340e3d0a3
cpawam <cyril.morin.tai@gmail.com>
2023-09-14 17:33:12 +0200

    DELETED port

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ import Title from './components/title';
 const localMode = false
 
 const localSRV = "http://localhost:3002";
-const liveSRV = "https://securechatapi.cyrilmorin.fr:3002";
+const liveSRV = "https://securechatapi.cyrilmorin.fr";
 const srv = localMode ? localSRV : liveSRV;
 
 if (!localMode) {
